46b516c946ce455c07f096bcc6be827176a7b842
Hammad Mazhar <16100032@lums.edu.pk>
2015-04-21 08:38:10 -0300

    Generated skeleton Rails

---
+++ EMSTrackerAPI/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EMSTrackerAPI::Application.config.secret_token = 'b0a6168cbf0b7a1043ecf74301a08f61958cbc63512206b4ce89963a846d0717dedcee078f9a6349367e6687d62dce7892aacb4a30e6e8705ba0bc1fb413858d'
